07fd6b6fda862bca89905d70d2b18c86603aee7b
diep ganguly <diep.ganguly@anu.edu.au>
2015-10-07 15:19:01 +1100

    cov to tdf for IGV single cytosine

--- merge_wigs.r
+++ merge_wigs.r
@@ -1,11 +1,9 @@
 # merge wig files
 
 files=dir(pattern="*.wig")
-
 data <- read.delim(files[1], head=F, skip=1)
 data <- data[,1:4]
 colnames(data)=c('V1','V2','V3',paste(files[1],'_prop',sep=''))
-
 for(i in 2:length(files)){
 file=read.delim(files[i],head=F,skip=1)
 file=file[,1:4]
@@ -13,7 +11,6 @@ colnames(file)=c('V1','V2','V3',paste(files[i],'_prop',sep=''))
 temp=merge(data,file,by=c('V1','V2','V3'),all=T)
 data=temp
 }
-
 # make all prop_met columns numeric
 for(i in 4:length(data)){
 data[,i] <- as.numeric(data[,i])
@@ -30,6 +27,6 @@ a=matrix(a,ncol=2,byrow=T)
 b=table(a[,1],a[,2])
 write.table(b,'test.txt',sep='\t',row.names=T,col.names=T)
 
-
-
+test=data[complete.cases(data),]
+cor(as.matrix(test[,4:5]))
 
