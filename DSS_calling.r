6fedad57c77222e446b05277b6728e995459857d
Diep <diep.ganguly@anu.edu.au>
2016-04-03 10:44:29 +1000

    simplify

--- DSS_calling.r
+++ DSS_calling.r
@@ -45,29 +45,13 @@ BSobj <- makeBSseqData(list(dat1.1,dat1.2,dat1.3,dat1.4,
 	      sampleNames=c("C1","C2","C3","C4",
 		 	    "N1","N2","N3","N4"))
 
-BSobj <- makeBSseqData(list(dat1.1,dat1.2,dat1.3,
-                            dat2.1,dat2.2,dat2.3),
-              sampleNames=c("C1","C2","C3",
-                            "N1","N2","N3"))
-
-BSobj <- makeBSseqData(list(dat1.1,dat1.2,dat2.1,dat2.2),
-              sampleNames=c("C1","C2","N1","N2"))
-
 ##################################
-# DML testing without smoothing
-dmlTest <- DMLtest(BSobj, group1=c("C1","C2","C3","C4"), group2=c("N1","N2","N3","N4"), smoothing=TRUE)
+# DML testing with smoothing moving averages and conservative smoothing window
+dmlTest <- DMLtest(BSobj, group1=c("C1","C2","C3","C4"), group2=c("N1","N2","N3","N4"), smoothing=TRUE, smoothing.span=50)
 ####################################
-
-#############################################################
-# perform dml testing with smoothing
-dmlTest <- DMLtest(BSobj, group1=c("C1","C2","C3"), group2=c("N1","N2","N3"), smoothing=TRUE)
-###############################################################
-dmlTest <- DMLtest(BSobj, group1=c("C1","C2"), group2=c("N1","N2"), smoothing=TRUE)
-
 
 # identify DMRs based on dmltesting
 dmrs <- callDMR(dmlTest, delta=0.1, minlen=50, minCG=3, pct.sig=0.5, dis.merge=50, p.threshold=pvalue)
-dmrs <- callDMR(dmlTest, delta=0.1, minlen=50, minCG=3, pct.sig=0.5, dis.merge=50, p.threshold=0.05)
 
 # write output. as bedfile so can use bedtools for later analyses
 file1=paste0(group1, "vs", group2, "_",context, "_", "p=", pvalue, ".bed")
