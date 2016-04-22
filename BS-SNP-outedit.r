51d77c3b0ab6d1992ceb76103d688027b1af75ab
Diep <diep.ganguly@anu.edu.au>
2016-04-22 11:29:38 +1000

    minor

--- BS-SNP-outedit.r
+++ BS-SNP-outedit.r
@@ -1,9 +1,10 @@
-fl = dir()
+fl = dir(pattern="SNP.out")
 for(i in 1:length(fl)){
 a <- read.delim(fl[i], skip=1)
 colnames(a)[1] = "#CHROM"
 a <- a[complete.cases(a),]
 a <- a[1:7]
+a <- a[order(a[,1], a[,2]),]
 write.table(a, file=paste0(fl[i],".edit"), sep='\t', quote=F, row.names=F)
 }
-.
+
