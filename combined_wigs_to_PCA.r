251988bdfbb5caaae9c623df441097735a12d44b
Diep <diep.ganguly@anu.edu.au>
2016-07-21 15:00:50 +1000

    BS-snper output edit

--- combined_wigs_to_PCA.r
+++ combined_wigs_to_PCA.r
@@ -1,15 +1,10 @@
 #!/usr/bin/env Rscript
-# R script to take all wig files in a directory to construct a correlation matrix 
-# between samples based on average methylation across 100bp windows. 
+# R script similar to wigs_to_PCA, except taking all contexts at once.
 # Then perform PCA.
-# Provide context of interest
 
 options(echo=T)
-args = commandArgs(trailingOnly=T)
-print(args)
-context=args[1]
 
-files=dir(pattern=paste0(context,"_100bp.wig"))
+files=dir(pattern=paste0("_100bp.wig"))
 data <- read.delim(files[1], head=F, skip=1)
 data <- data[,1:4]
 
@@ -27,21 +22,19 @@ test=data[complete.cases(data),]
 # Correlation matrix
 a <- cor(as.matrix(test[,4:length(test)]))
 
-name = as.numeric(regexec(pattern="_C", text=colnames(a)))
+name = as.numeric(regexec(pattern="_100", text=colnames(a)))
 colnames(a) = substr(x=colnames(a), start=1, stop=name-1)
 
-# Correlation as distance
+# Use correlation as distance
 dd <- as.dist(a)
+names(dd) = colnames(a)
 
-# PCA analysis
+# PCA 
 pc=prcomp(a)
 
-# Make pdf figure
-pdf(file=paste0("100bp_",context,"_hclust_PCA.pdf"))
-plot(hclust(dd), main='hclust on 100bp wigs', cex = 0.5)
+pdf(file=paste0("100bp_combined_hclust_PCA.pdf"))
+plot(hclust(dd), main='hclust on 100bp wigs', cex=0.5)
 plot(pc, type ='l' , main='Variance of PCs')
 plot(pc$x[1,], pc$x[2,], xlab = 'PC1', ylab='PC2', main = 'PCA on 100bp wigs', cex=0.5)
 text(pc$x[1,], pc$x[2,], colnames(a), cex = 0.5, pos=4)
 dev.off()
-
-
