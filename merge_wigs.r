ceb442ab22c84099bf1efe1aa6b00c4e9c03fb3e
diep ganguly <diep.ganguly@anu.edu.au>
2016-08-12 08:38:46 +1000

    100bp genome correlations

--- merge_wigs.r
+++ merge_wigs.r
@@ -1,31 +1,43 @@
 # merge wigs to make pairwise correlation matrices
+# run in directory with wig files to correlate
 
 files=dir(pattern="*.wig")
 data <- read.delim(files[1], head=F, skip=1)
 data <- data[,1:4]
-colnames(data)=c('V1','V2','V3',paste(files[1],'_prop',sep=''))
+data <- data[data$V1 != 'Mt',]
+data <- data[data$V1 != 'Pt',]
+test <- as.numeric(regexec(text=paste0(files[1]), pattern='_C'))
+name <- substr(paste0(files[1]), start=1, stop=test-1)
+colnames(data)=c('V1','V2','V3',paste(name))
 for(i in 2:length(files)){
 file=read.delim(files[i],head=F,skip=1)
 file=file[,1:4]
-colnames(file)=c('V1','V2','V3',paste(files[1],'_prop',sep=''))
+file <- file[file$V1 != 'Mt',]
+file <- file[file$V1 != 'Pt',]
+test <- as.numeric(regexec(text=paste0(files[i]), pattern='_C'))
+name <- substr(paste0(files[i]), start=1, stop=test-1)
+colnames(file)=c('V1','V2','V3',paste(name))
 temp=merge(data,file,by=c('V1','V2','V3'),all=T)
 data=temp
 }
-# make all prop_met columns numeric
-for(i in 4:length(data)){
-data[,i] <- as.numeric(data[,i])
-}
-
-# psuedo correlation matrix
-
-data=read.delim('output.txt',head=T)
-cg=data[grep("*CpG",data$contrast),]
-a=strsplit(as.character(cg$contrast),"vs")
-a=unlist(a)
-a=matrix(a,ncol=2,byrow=T)
-
-b=table(a[,1],a[,2])
-write.table(b,'test.txt',sep='\t',row.names=T,col.names=T)
 
 test=data[complete.cases(data),]
-write.table(cor(as.matrix(test[,4:length(test)])), 'test.txt', sep='\t', row.names=T, col.names=T, quote=F)
+a <- cor(as.matrix(test[,4:length(test)]))
+write.table(a, 'correlation_matrix.txt', sep='\t', row.names=T, col.names=T, quote=F)
+
+b <- a[hc$order, hc$order]
+write.table(b, 'correlation_matrix_hc_ordered.txt', sep='\t', row.names=T, col.names=T, quote=F)
+
+pdf("cor.pdf")
+library(gplots)
+heatmap.2(a,
+          trace = 'none',
+          density.info = "none",
+          symm = F,
+          symkey = F,
+          symbreaks = T,
+          key = T,
+          dendrogram='both',
+          )
+dev.off()
+
