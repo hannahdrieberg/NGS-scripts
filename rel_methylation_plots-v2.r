ce165eb9938c74d41cb99eb90c8e97585f028f84
diep ganguly <diep.ganguly@anu.edu.au>
2017-02-22 10:27:43 +1100

    working

--- rel_methylation_plots-v2.r
+++ rel_methylation_plots-v2.r
@@ -12,7 +12,7 @@ if(context=="CHH"){seq=list("CAA", "CAT", "CCA", "CCC", "CCT", "CTA", "CTC", "CT
 out <- NULL
 for(i in seq){
 # read in files
-a <- read.delim(paste0(smplname,'-',outname,'-',context,'-',i,'1k.bed'), head=F)
+a <- read.delim(paste0(smplname,'-',outname,'-',context,'-',i,'.1k.bed'), head=F)
 a <- subset(a,a$V1!='Mt' & a$V1!='Pt')
 a <- a[c(1:3, 7,8,12,14:20)]
 real.dist=matrix(ifelse(a$V19=='+',-1*a$V20,a$V20),ncol=1)
@@ -24,7 +24,10 @@ a$rel.dist=fixy
 a.bin=stats.bin(a$rel.dist,a$V12,N=100)
 p.a.bin=cbind(matrix(a.bin$centers,ncol=1),a.bin$stats["mean",])
 test <- as.data.frame(p.a.bin)
-colnames(test)=c('pos',paste(smplname,'-',i,sep=''))
-out <- cbind(out,test)
+colnames(test)[1] <- 'pos'
+test$sample <- paste0(smplname)
+test$context <- paste0(context)
+test$motiff <- paste0(i)
+out <- rbind(out,test)
 }
-write.table(out,paste(smplname,'_',context,'_',outname,'.txt',sep=''),sep='\t')
+write.table(out,paste(smplname,'_',context,'_',outname,'.txt',sep=''),quote=F, col.names=T, row.names=F, sep='\t')
