d5315dfe06046b1ed6d7963610a4baac815535be
diep ganguly <diep.ganguly@anu.edu.au>
2017-02-22 15:30:38 +1100

    minor

--- met-sign.sh
+++ met-sign.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 set -e
+
 # re-extract DNA methylation, at custom sequence contexts, from sam file and produce cytosine report
 # perform in 4_bismark output sub-directory of wgbs workflow
+
 if [ "$#" -ne 6 ]; then
 echo "USAGE: met-sign.sh <context> <file> <file path to met bedfile> <annotation file> <sample> <outname>"
 exit 1
 fi
+
 context=$1
 fl=$2
 bedfile=$3
 annopath=$4
 sample=$5
 outname=$6
+
 echo "Extracting CX report from SAM ..."
+
 bismark_methylation_extractor --comprehensive --cytosine_report --CX --genome_folder ~/TAIR10_bs/  --report --buffer_size 8G -s ${fl}
+
 echo "done"
 echo "grepping & bedtools ..."
+
 if [ ${context} = CHH ]; then
 seq="CAA CAC CAT CCA CCC CCT CTA CTC CTT"
 fi
+
 echo ${context}
 echo ${fl}
 echo ${bedfile}
@@ -26,8 +34,11 @@ echo ${annopath}
 echo ${sample}
 echo ${outname}
 echo ${seq}
+
 awk '{print $1 "\t" $2 "\t" $2+1 "\t" $3 "\t" $4 "\t" $5 "\t" $6 "\t" $7}' ${fl::-3}.CX_report.txt > ${fl::-3}.CX_report.bed
+
 # use grep to get output of specific sequence context from report files
+
 for FILE in $seq
 do
 grep ${FILE} ${fl::-3}.CX_report.bed > ${FILE}.bed
@@ -41,12 +52,20 @@ rm ${context}-${FILE}.bed
 rm sorted-${context}-${FILE}.bed
 rm ${outname}-${context}-${FILE}.bed
 done
+
 echo "done"
 echo "cleaning ..."
+
 rm *bedGraph.gz
 rm *cov.gz
 rm *context*bismark.sam.gz.txt
 rm *M-bias.txt
 rm *splitting_report.txt
+
 echo "done ... r plotting"
+
 Rscript ~/scripts/rel_methylation_plots-v2.r ${sample} ${outname} {context}
+
+rm *1k.bed
+rm *CX*
+echo "DONE"
--- rel_methylation_plots-v2.r
+++ rel_methylation_plots-v2.r
@@ -15,6 +15,7 @@ for(i in seq){
 a <- read.delim(paste0(smplname,'-',outname,'-',context,'-',i,'.1k.bed'), head=F)
 a <- subset(a,a$V1!='Mt' & a$V1!='Pt')
 a <- a[c(1:3, 7,8,12,14:20)]
+a <- subset(a,a$V20 != -1)
 real.dist=matrix(ifelse(a$V19=='+',-1*a$V20,a$V20),ncol=1)
 a=cbind(a,real.dist)
 rel.dist=matrix(ifelse(a$real.dist==0,ifelse(a$V19=="-",((a$V16 - a$V2)/(a$V16 - a$V15))*1000,((a$V2 - a$V15)/(a$V16 - a$V15))*1000),ifelse(a$real.dist>0,a$real.dist + 1000,a$real.dist)),ncol=1)
