26eca4d9e369c3420c667f7a85ee81bfdebfd1aa
diep ganguly <diep.ganguly@anu.edu.au>
2017-06-01 12:25:22 +1000

    remove finish line

--- BS-SNPer.sh
+++ BS-SNPer.sh
@@ -17,4 +17,4 @@ file=$1
 perl ~/bin/BS-Snper-master/BS-Snper.pl --fa ~/TAIR10_bs/Arabidopsis_thaliana.TAIR10.22.dna.genome.fa --input ${file}_trimmed.fq_bismark.sorted.bam --output temp.out --methcg meth.cg --methchg meth.chg --methchh meth.chh --minhetfreq 0.15 --minhomfreq 0.85 --minquali 30 --mincover 15 --maxcover 1000 --minread2 2 --errorate 0.02 --mapvalue 20 > ${file}.SNP.bed 2>${file}_ERR.log
 
 sort -k1,1 -k2,2n ${file}.SNP.bed > ${file}.SNP.sorted.bed
-
+sed -i '/SNP finished/d' ${file}.SNP.sorted.bed
