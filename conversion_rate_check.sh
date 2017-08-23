2b33326571706aedc858046aec8d63ef95fb512f
diep ganguly <diep.ganguly@anu.edu.au>
2017-08-23 13:52:52 +1000

    pick file to analyse

--- conversion_rate_check.sh
+++ conversion_rate_check.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+set -eu
+# Get sodium bisulfite conversion efficiency by calculating percent unconverted C's in Cp
+
+echo "Conversion % in $1"
+
+file=$1
 
 echo "chrC"
-grep "C"  *CHH.bed.bismark.cov | awk '{ met+= $5} { unmet += $6} { total = met + unmet } END {print 100-((met / total)*100)}'
+grep "C"  ${file} | awk '{ met+= $5} { unmet += $6} { total = met + unmet } END {print 100-((met / total)*100)}'
 echo "chrPt"
-grep "Pt"  *CHH.bed.bismark.cov | awk '{ met+= $5} { unmet += $6} { total = met + unmet } END {print 100-((met / total)*100)}'
+grep "Pt"  ${file} | awk '{ met+= $5} { unmet += $6} { total = met + unmet } END {print 100-((met / total)*100)}'
 echo "chrMt"
-grep "Mt"  *CHH.bed.bismark.cov | awk '{ met+= $5} { unmet += $6} { total = met + unmet } END {print 100-((met / total)*100)}'
+grep "Mt"  ${file} | awk '{ met+= $5} { unmet += $6} { total = met + unmet } END {print 100-((met / total)*100)}'
