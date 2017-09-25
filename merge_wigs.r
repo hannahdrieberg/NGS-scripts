1bf8141c8b2d8b7a6fdd72cf099882d159fb7531
diep ganguly <diep.ganguly@anu.edu.au>
2017-09-26 08:54:05 +1000

    re-formatting

--- merge_wigs.r
+++ merge_wigs.r
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
-# merge wigs to make pairwise correlation matrices
+# merge wigs to make correlation matrices
 # run in directory with wig files to correlate
 
 options(echo=T)
@@ -7,36 +7,28 @@ args = commandArgs(trailingOnly=T)
 print(args)
 context=args[1]
 
+require(reshape2)
+require(gplots)
+
 files=dir(pattern=paste0(context,"_100bp.wig"))
 
-data <- read.delim(files[1], head=F, skip=1)
+out <- NULL
+
+for(i in 1:length(files)){
+data <- read.delim(files[i],head=F,skip=1)
 data <- data[,1:4]
 data <- data[data$V1 != 'Mt' & data != "ChrM" & data != "M",]
 data <- data[data$V1 != 'Pt' & data != "ChrC" & data != "C",]
-test <- as.numeric(regexec(text=paste0(files[1]), pattern='_C'))
-name <- substr(paste0(files[1]), start=1, stop=test-1)
-colnames(data)=c('V1','V2','V3',paste(name))
-for(i in 2:length(files)){
-file=read.delim(files[i],head=F,skip=1)
-file=file[,1:4]
-file <- file[file$V1 != 'Mt',]
-file <- file[file$V1 != 'Pt',]
-test <- as.numeric(regexec(text=paste0(files[i]), pattern='_C'))
-name <- substr(paste0(files[i]), start=1, stop=test-1)
-colnames(file)=c('V1','V2','V3',paste(name))
-temp=merge(data,file,by=c('V1','V2','V3'),all=T)
-data=temp
+data <- data[complete.cases(data),]
+data$sample <- sapply(strsplit(files[i], '_'), function(l) l[1])
+data$context <- sapply(strsplit(files[i], '_'), function(l) l[2])
+data$V1 <- ifelse(substr(data$V1, start=1, stop=3) == "Chr", paste0(data$V1),paste0("Chr",data$V1))
+out <- rbind(out,data)
 }
 
-test=data[complete.cases(data),]
+test <- dcast(out, formula=V1+V2+V3~sample, value.var = "V4")
+test <- test[complete.cases(test),]
 a <- cor(as.matrix(test[,4:length(test)]))
-
-# output correlation matrix with hierarchical clustering 
-# hc <- hclust(as.dist(a))
-# b <- a[hc$order, hc$order]
-# write.table(b, 'correlation_matrix_hc_ordered.txt', sep='\t', row.names=T, col.names=T, quote=F)
-
-library(gplots)
 
 pdf(file=paste0('wig_cor_',context,'.pdf'), width=8, height = 7.5, pointsize = 10)
 heatmap.2(a, 
@@ -48,9 +40,8 @@ heatmap.2(a,
 	colsep = 1:ncol(a),
 	rowsep = 1:nrow(a),
 	sepcolor = "white",
-	sepwidth = c(0.01,0.001),
+	sepwidth = c(0.001,0.001),
 	dendrogram='both',
 	cexCol=0.8,
 	cexRow=0.8)
 dev.off()
-
