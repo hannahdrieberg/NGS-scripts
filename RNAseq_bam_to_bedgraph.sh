8e76ed34215951f7b72c00c58a48abf9f497d6a6
diep ganguly <diep.ganguly@anu.edu.au>
2017-10-04 16:09:58 +1100

    plus/minus strand shenanigans; redefined input

--- RNAseq_bam_to_bedgraph.sh
+++ RNAseq_bam_to_bedgraph.sh
@@ -8,84 +8,94 @@ set -u
 # Ensure subread indexed genome & chromosome sizes are prepared eg TAIR10/TAIR10_Chr.all.fasta.len
 # samtools faidx TAIR10_Chr.all.fasta | cut -f1,2 TAIR10_Chr.all.fasta.fai > TAIR10_Chr.all.fasta.len
 
-if [ "$#" -lt 2 ]; then
+if [ "$#" -lt 3 ]; then
 echo "Missing arguments!"
-echo "USAGE: bam_to_bedgraph.sh <sample name> <unstranded, SE_stranded, PE_stranded>"
-echo "EXAMPLE: RNAseq_bam_to_bedgraph.sh col0-r1 unstranded"
+echo "USAGE: bam_to_bedgraph.sh <sample name> <SE, PE> <unstranded, stranded>"
+echo "EXAMPLE: RNAseq_bam_to_bedgraph.sh col0-r1 PE unstranded"
 exit 1
 fi
 
 smp="$1.sorted.bam"
 lay=$2
+str=$3
 
 # file for length of all 7 chromosomes
 chrc_sizes=/home/diep/TAIR10/TAIR10_Chr.all.fasta.len
 
+echo ""
 echo "sample = $1"
 echo "layout = $2"
+echo "strand = $3"
+echo ""
+echo "Produce $lay $str bigWig file(s) from $smp ..."
+echo ""
 
-echo "Produce ${2} tiled data files from ${smp} ..."
+if [[ "$lay" == "SE" ]] && [[ "$str"  == "unstranded" ]] ; then 
 
-echo "BAM to bedgraph"
+echo "BAM to bedgraph ..."
+# unstranded bedgraph
+bedtools genomecov -bga -split -ibam $smp -g $chrc_sizes > ${smp%%sorted.bam*}.bg
 
-if [[ "$lay" == "unstranded" ]]; then
-echo "non-stranded bedgraph"
-# non-stranded bedgraph
-bedtools genomecov -bga -split -ibam $smp -g $chrc_sizes > ${smp%%sorted.bam*}.bedgraph | tee -a ../${1}*.log
-
-# non-stranded bedgraph with splicing & nt resolution
-bedtools genomecov -d -split -ibam $smp -g $chrc_sizes > ${smp%%sorted.bam*}.bed | tee -a ../${1}*.log
-
-echo "TDFs..."
-igvtools toTDF ${smp%%sorted.bam*}.bedgraph $${smp%%sorted.bam*}.tdf $chrc_sizes
+# bg to bigWig
+echo "bigWig ..."
+/home/diep/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}bg ${chrc_sizes} ${smp%%bam}bigWig
 
 fi
 
-if [[ "$lay" == "SE_stranded" ]];then
-echo "stranded SE bedgraph"
-echo "to be built"
 
+if [[ "$lay" == "SE" ]] && [[ "$str"  == "stranded" ]] ; then
+
+echo "Does not exist ....bye"
 # https://www.biostars.org/p/179035/
-
 # samtools view -f 128 F 4 -b $smp > 
 
 fi
 
-if [[ "$lay" == "PE_stranded" ]];then
-echo "stranded PE bedgraph"
-echo "Extract properly-paired reads and their mates (ie flags 99/147/163/83) from paired-end BAM files"
-# Extract properly-paired reads and their mates (ie flags 99/147/163/83) from paired-end BAM files
-# https://gist.github.com/mtw/7175143
+
+if [[ "$lay" == "PE" ]] && [[ "$str"  == "unstranded" ]] ; then
+
+echo "BAM to bedgraph ..."
+# unstranded bedgraph
+bedtools genomecov -bga -split -ibam $smp -g $chrc_sizes > ${smp%%sorted.bam*}.bg
+
+# bg to bigWig
+echo "bigWig ..."
+/home/diep/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}bg ${chrc_sizes} ${smp%%bam}bigWig
+
+fi
+
+
+if [[ "$lay" == "PE" ]] && [[ "$str"  == "stranded" ]] ; then
+
+echo "Extract properly-paired reads and their mates (+ flags 99/147; - flags 83/163) from paired-end BAM files"
 # http://seqanswers.com/forums/showthread.php?t=29399
-# stranded PE bams
 
-#R1 forward strand
-samtools view -f 99 -b ${smp} > ${smp%%bam}R1F.bam 2>&1 | tee -a ../${1}*.log
-#R2 reverse strand
-samtools view -f 147 -b ${smp} > ${smp%%bam}R2R.bam 2>&1 | tee -a ../${1}*.log
-#FORWARD '+' reads
-samtools merge -f ${smp%%bam}forward.bam ${smp%%bam}R1F.bam ${smp%%bam}R2R.bam 2>&1 | tee -a ../${1}*.log
+# R1 forward
+samtools view -f 99 -b ${smp} > ${smp%%bam}R1F.bam
+# R2 reverse
+samtools view -f 147 -b ${smp} > ${smp%%bam}R2R.bam
+# FORWARD R1 read pairs
+samtools merge -f ${smp%%bam}forward.bam ${smp%%bam}R1F.bam ${smp%%bam}R2R.bam
 
-#R1 reverse strand
-samtools view -f 83 -b ${smp} > ${smp%%bam}R1R.bam | tee -a ../${1}*.log
-#R2 forward strand
-samtools view -f 163 -b ${smp} > ${smp%%bam}R2F.bam | tee -a ../${1}*.log
-# REVERSE '-' reads
-samtools merge -f ${smp%%bam}reverse.bam ${smp%%bam}R1R.bam ${smp%%bam}R2F.bam | tee -a ../${1}*.log
-# clean
-rm ${smp%%bam}*R*bam -v
+# R1 reverse
+samtools view -f 83 -b ${smp} > ${smp%%bam}R1R.bam
+# R2 forward
+samtools view -f 163 -b ${smp} > ${smp%%bam}R2F.bam
+# REVERSE R1 read pairs
+samtools merge -f ${smp%%bam}reverse.bam ${smp%%bam}R1R.bam ${smp%%bam}R2F.bam
 
-echo "Produce ${2} tiled data files from ${smp} ..."
-
-echo "BAM to bedgraph"
-# stranded bedgraphs - do not use '-strand +' flag to allow accounting of PE reads
-# minus strand
-bedtools genomecov -bga -split -scale -1 -ibam ${smp%%bam}reverse.bam -g $chrc_sizes > ${smp%%bam}minus.bg
+echo "BAM to stranded bedgraph ..."
 # plus strand
-bedtools genomecov -bga -split -ibam ${smp%%bam}forward.bam -g $chrc_sizes > ${smp%%bam}plus.bg
+bedtools genomecov -bga -split -ibam ${smp%%bam}reverse.bam -g $chrc_sizes > ${smp%%bam}plus.bg
+# minus strand
+bedtools genomecov -bga -split -scale -1 -ibam ${smp%%bam}forward.bam -g $chrc_sizes > ${smp%%bam}minus.bg
 
 echo "bigWigs..."
 /home/diep/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}plus.bg ${chrc_sizes}  ${smp%%bam}plus.bigWig
 /home/diep/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}minus.bg ${chrc_sizes} ${smp%%bam}minus.bigWig
+
+# clean
+rm ${smp%%bam}*bam -v
+rm ${smp%%bam}*bg -v
 
 fi
