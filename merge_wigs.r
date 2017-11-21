839bc9b6082124e34785665c8add7037cb13fc76
diep ganguly <diep.ganguly@anu.edu.au>
2017-11-21 11:46:59 +1100

    tidied up and summarises reps

--- merge_wigs.r
+++ merge_wigs.r
@@ -2,43 +2,41 @@
 # merge wigs to make correlation matrices
 # run in directory with wig files to correlate
 
-options(echo=T)
 args = commandArgs(trailingOnly=T)
 print(args)
 context=args[1]
 
-require(reshape2)
-require(gplots)
+library(tidyverse)
+library(gplots)
 
-files=dir(pattern=paste0(context,"_100bp.wig"))
-
-out <- NULL
-
-for(i in 1:length(files)){
-data <- read.delim(files[i],head=F,skip=1)
-data <- data[,1:4]
-data <- data[data$V1 != 'Mt' & data != "ChrM" & data != "M",]
-data <- data[data$V1 != 'Pt' & data != "ChrC" & data != "C",]
-data <- data[complete.cases(data),]
-data$sample <- sapply(strsplit(files[i], '_'), function(l) l[1])
-data$context <- sapply(strsplit(files[i], '_'), function(l) l[2])
-data$V1 <- ifelse(substr(data$V1, start=1, stop=3) == "Chr", paste0(data$V1),paste0("Chr",data$V1))
-out <- rbind(out,data)
-}
-
-test <- dcast(out, formula=V1+V2+V3~sample, value.var = "V4")
-test <- test[complete.cases(test),]
-a <- cor(as.matrix(test[,4:length(test)]))
+files <- dir(pattern=paste0(context,"_100bp.wig"))
+data <- data_frame(files) %>%
+mutate(file_contents = map(files, read_delim, delim='\t', col_names=F, skip=1)) %>%
+unnest() %>%
+filter(X1!='Mt'&X1!='ChrM'&X1!='Pt'&X1!='ChrC') %>%
+mutate(sample=sapply(strsplit(files, '_'), function(l) l[1])) %>%
+mutate(genotype=sapply(strsplit(sample, '-'), function(l) l[1])) %>%
+mutate(rep=sapply(strsplit(sample, '-'), function(l) l[2])) %>%
+mutate(X1=ifelse(substr(X1, start=1, stop=3)=="Chr",paste0(X1),paste0("Chr",X1))) %>%
+na.omit() %>%
+group_by(X1, X2, X3, genotype) %>%
+summarise(met = mean(X4)) %>%
+spread(key=genotype, value=met) %>%
+na.omit() %>%
+ungroup() %>%
+select(-X1, -X2, -X3) %>%
+cor() %>%
+as.matrix()
 
 pdf(file=paste0('wig_cor_',context,'.pdf'), width=8, height = 7.5, pointsize = 10)
-heatmap.2(a, 
+heatmap.2(data, 
 	trace='none',
 	density.info='none',
 	symm=F,
 	symkey=F,
 	key=T,
-	colsep = 1:ncol(a),
-	rowsep = 1:nrow(a),
+	colsep = 1:ncol(data),
+	rowsep = 1:nrow(data),
 	sepcolor = "white",
 	sepwidth = c(0.001,0.001),
 	dendrogram='both',
