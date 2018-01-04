8b183766499ebdb48b8472f06f9edd6c5efa4d24
diep ganguly <diep.ganguly@anu.edu.au>
2018-01-04 13:01:32 +1100

    update anno

--- DSS_file_prep.r
+++ DSS_file_prep.r
@@ -16,8 +16,8 @@ files=dir(pattern=paste0(context,".bed.bismark.cov"))
 ## get total and met counts for each sample in given context and write out as separate files
 for(i in 1:length(files)){
 file <- read.delim(files[i], head=F)
-file <- file[file$V1 != "Mt",]
-file <- file[file$V1 != "Pt",]
+file <- file[file$V1 != "ChrM",]
+file <- file[file$V1 != "ChrC",]
 file[,7] <- file[,5] + file[,6]
 file <- file[,c(1,2,7,5)]
 test <- as.numeric(regexec(text = as.character(files[i]), pattern=".bed"))
