e292630b456ea800a93a8a453001501ecf5e68d6
diep ganguly <diep.ganguly@anu.edu.au>
2018-03-12 11:49:07 +1100

    chr -> Chr & plotting

--- MethylC/methimpute.r
+++ MethylC/methimpute.r
@@ -1,3 +1,8 @@
+options(echo=T)
+library(fields)
+args=commandArgs(trailingOnly=T)
+print(args)
+
 ## Perform METHimpute
 # https://github.com/ataudt/methimpute/blob/master/README.md
 # https://github.com/ataudt/methimpute/blob/master/vignettes/methimpute.pdf
@@ -11,13 +16,13 @@
 
 ### Input files
 # Run in bash to get 1-based genome-wide cytosine report
-# bismark_methylation_extractor --cytosine_report --CX --genome_folder /home/diep/TAIR10 *sorted.bam
+# bismark_methylation_extractor --multicore 4 --cytosine_report --CX --genome_folder /home/diep/TAIR10 *sorted.bam
 
 ## load library
 library(methimpute)
 
-## actual files
-file <- dir(pattern="*.CX_report.txt")
+## file "CX_report.txt"
+file <- args[1]
 
 ## chromosome lengths from methimpute
 data(arabidopsis_chromosomes)
@@ -30,18 +35,51 @@ bismark.data <- importBismark(file, chrom.lengths=arabidopsis_chromosomes)
 fasta.file <- '/home/diep/TAIR10/arabidopsis_seq.fa'
 cytosine.positions = extractCytosinesFromFASTA(fasta.file, contexts = c('CG','CHG','CHH'))
 methylome = inflateMethylome(bismark.data,cytosine.positions)
-
 print(methylome)
 
 ## obtain correlation parameters (methylation levels from adjacent cytosines)
 distcor = distanceCorrelation(methylome)
+# estimate decay parameter for distancce dependeny of the transition probabilities in HMM
 fit = estimateTransDist(distcor)
 
 print(fit)
 dev.off()
 
+## HMM for complete set using transition probabilities
 model = callMethylation(data = methylome, transDist = fit$transDist)
 print(model)
 
-outname <- sapply(strsplit(file, "_"), function(l) l[1])
-exportMethylome(model, paste0(outname,".tsv"))
+## Per context HMMs
+# model <- callMethylationSeparate(data=methylome)
+# print(model)
+
+### METHimpute plotting
+pdf("Methimpute_plotting.pdf")
+plotHistogram(model, total.counts=5)
+plotScatter(model)
+plotTransitionProbs(model)
+plotConvergence(model)
+plotPosteriorDistance(model$data)
+dev.off()
+
+# At genes and TE coordinates
+data(arabidopsis_genes)
+seqlevels(arabidopsis_genes) <-  sub('chr', 'Chr', seqlevels(arabidopsis_genes))
+data(arabidopsis_TEs)
+seqlevels(arabidopsis_TEs) <- sub('chr', 'Chr', seqlevels(arabidopsis_TEs))
+
+# At enrichment plots
+pdf("METHimpute_enrichments_plots.pdf")
+plotEnrichment(model, annotation=arabidopsis_genes)
+plotEnrichment(model, annotation=arabidopsis_TEs)
+dev.off()
+
+## Bin cytosine positions, methylation counts in equidistant windows
+# binMethylome(data, binsize, contexts = "total", columns.average = c("rc.meth.lvl"))
+
+## Output recalibrated methylation levels
+exportMethylome(model, filename = paste0(outname,".tsv"))
+CG_out <- model$data[model$data$context == "CG"]
+CHG_out <- model$data[model$data$context == "CHG"]
+CHH_out <- model$data[model$data$context == "CHH"]
+
