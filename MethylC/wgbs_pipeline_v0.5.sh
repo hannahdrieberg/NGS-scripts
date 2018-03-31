deeb614223c6d114d5a4e4a584ce267da3455d44
Diep Ganguly <diepg@flashheart>
2018-03-31 16:55:13 +1100

    v0.16 bismark output file name shenanigans

--- MethylC/wgbs_pipeline_v0.5.sh
+++ MethylC/wgbs_pipeline_v0.5.sh
@@ -5,6 +5,7 @@ set -u
 # This script it designed to take a single end fastq file and process it through the 
 # bismark aligner call methylated cytosines, and develop per-c bed files and 100bp 
 # window wig files for CG, CHG, and CHH methylation levels
+# Compatible with Bismark v0.16.0
 # USES BOWTIE2 FOR ALIGNMENT
 # Genome indexing
 # Bowtie2: bismark_genome_preparation --bowtie2 /path/to/genome
@@ -72,7 +73,7 @@ mv 2_trimgalore/${fq_file%%.fastq*}_trimmed_fastqc* 3_trimmed_fastqc
 mkdir 0_rawfastq
 mv $fq_file 0_rawfastq
 
-#bismark to BAM
+## bismark to BAM
 mkdir 4_bismark_alignment
 cd 4_bismark_alignment
 
@@ -83,18 +84,19 @@ samtools index ${fq_file%%.fastq*}_trimmed_bismark.sorted.bam 2>&1 | tee -a ../$
 
 rm -v *bismark_bt2.bam
 
-#methylation extraction
+## methylation extraction
 bismark_methylation_extractor --comprehensive --report --multicore 2 --buffer_size 8G -s ${fq_file%%.fastq*}_trimmed_bismark.sorted.bam 2>&1 | tee -a ../${fileID}_logs_${dow}.log
 
-#bedgraph creation
+## bedgraph creation
 bismark2bedGraph --CX CpG* -o ${fileID}_CpG.bed 2>&1 | tee -a ../${fileID}_logs_${dow}.log
 bismark2bedGraph --CX CHG* -o ${fileID}_CHG.bed 2>&1 | tee -a ../${fileID}_logs_${dow}.log
 bismark2bedGraph --CX CHH* -o ${fileID}_CHH.bed 2>&1 | tee -a ../${fileID}_logs_${dow}.log
+
 cd ../
 mkdir 5_output_files
 mv 4_bismark_alignment/*.bed* 5_output_files
 
-#100bp window creation
+## 100bp window creation
 perl $HOME/scripts/MethylC/C_context_window_SREedits.pl 4_bismark_alignment/CpG* 100 0 ${fileID}_CpG 2>&1 | tee -a ${fileID}_logs_${dow}.log
 mv 4_bismark_alignment/CpG*.wig 5_output_files/${fileID}_CpG_100bp.wig
 perl $HOME/scripts/MethylC/C_context_window_SREedits.pl 4_bismark_alignment/CHG* 100 0 ${fileID}_CHG 2>&1 | tee -a ${fileID}_logs_${dow}.log
@@ -110,13 +112,13 @@ echo "#####################"
 bismark_version=$(bismark --version | grep "Bismark Version:" | cut -d":" -f2 | tr -d ' ')
 samtools_version=$(samtools 3>&1 1>&2 2>&3 | grep "Version:" | cut -d' ' -f2 | tr -d ' ')
 
-map_ef=$(grep 'Mapping efficiency:' 4_bismark_alignment/${fq_file%%.fastq*}_trimmed.fq*_bismark_bt2_SE_report.txt  | cut -d: -f2 | tr -d '\t' | cut -d'%' -f1)
-unique_aln=$(grep 'Number of alignments with a unique best hit from the different alignments:' 4_bismark_alignment/${fq_file%%.fastq*}_trimmed.fq*_bismark_bt2_SE_report.txt  | cut -d: -f2 | tr -d '\t')
-no_aln=$(grep 'Sequences with no alignments under any condition:' 4_bismark_alignment/${fq_file%%.fastq*}_trimmed.fq*_bismark_bt2_SE_report.txt  | cut -d: -f2 | tr -d '\t')
-multi_aln=$(grep 'Sequences did not map uniquely:' 4_bismark_alignment/${fq_file%%.fastq*}_trimmed.fq*_bismark_bt2_SE_report.txt  | cut -d: -f2 | tr -d '\t')
-cpg_per=$(grep 'C methylated in CpG context:' 4_bismark_alignment/${fq_file%%.fastq*}_trimmed.fq*_bismark_bt2_SE_report.txt  | cut -d: -f2 | tr -d '\t' | cut -d'%' -f1)
-chg_per=$(grep 'C methylated in CHG context:' 4_bismark_alignment/${fq_file%%.fastq*}_trimmed.fq*_bismark_bt2_SE_report.txt  | cut -d: -f2 | tr -d '\t' | cut -d'%' -f1)
-chh_per=$(grep 'C methylated in CHH context:' 4_bismark_alignment/${fq_file%%.fastq*}_trimmed.fq*_bismark_bt2_SE_report.txt  | cut -d: -f2 | tr -d '\t' | cut -d'%' -f1)
+map_ef=$(grep 'Mapping efficiency:' 4_bismark_alignment/${fq_file%%.fastq*}_trimmed_bismark_bt2_SE_report.txt  | cut -d: -f2 | tr -d '\t' | cut -d'%' -f1)
+unique_aln=$(grep 'Number of alignments with a unique best hit from the different alignments:' 4_bismark_alignment/${fq_file%%.fastq*}_trimmed_bismark_bt2_SE_report.txt  | cut -d: -f2 | tr -d '\t')
+no_aln=$(grep 'Sequences with no alignments under any condition:' 4_bismark_alignment/${fq_file%%.fastq*}_trimmed_bismark_bt2_SE_report.txt  | cut -d: -f2 | tr -d '\t')
+multi_aln=$(grep 'Sequences did not map uniquely:' 4_bismark_alignment/${fq_file%%.fastq*}_trimmed_bismark_bt2_SE_report.txt  | cut -d: -f2 | tr -d '\t')
+cpg_per=$(grep 'C methylated in CpG context:' 4_bismark_alignment/${fq_file%%.fastq*}_trimmed_bismark_bt2_SE_report.txt  | cut -d: -f2 | tr -d '\t' | cut -d'%' -f1)
+chg_per=$(grep 'C methylated in CHG context:' 4_bismark_alignment/${fq_file%%.fastq*}_trimmed_bismark_bt2_SE_report.txt  | cut -d: -f2 | tr -d '\t' | cut -d'%' -f1)
+chh_per=$(grep 'C methylated in CHH context:' 4_bismark_alignment/${fq_file%%.fastq*}_trimmed_bismark_bt2_SE_report.txt  | cut -d: -f2 | tr -d '\t' | cut -d'%' -f1)
 
 if [[ $fq_file == *gz* ]];then
 	raw_reads=$(zcat 0_rawfastq/*.gz | wc -l)
@@ -138,7 +140,7 @@ if [[ $fq_file != *gz* ]];then
 	flt_reads=$(($flt_reads / 4))
 fi
 
-#add it to the full pipeline logfile
+## add it to the full pipeline logfile
 printf "${dow}\t${fq_file}\t${fileID}\t${genome_path/}\t${type}\t${bismark_version}\t${samtools_version}\t${raw_reads}\t${flt_reads}\t${map_ef}\t${unique_aln}\t${no_aln}\t${multi_aln}\t${cpg_per}\t${chg_per}\t${chh_per}\n"
 printf "${dow}\t${fq_file}\t${fileID}\t${genome_path/}\t${type}\t${bismark_version}\t${samtools_version}\t${raw_reads}\t${flt_reads}\t${map_ef}\t${unique_aln}\t${no_aln}\t${multi_aln}\t${cpg_per}\t${chg_per}\t${chh_per}\n" >> $HOME/wgbs_se_pipeline_analysis_record.log
 
@@ -207,16 +209,16 @@ mv $fq_file2 0_rawfastq
 mkdir 4_bismark_alignment
 cd 4_bismark_alignment
 
-#PE alignment
+## PE alignment
 bismark --un $genome_path -1 ../2_trimgalore/${fq_file1%%.fastq*}_val_1.fq* -2 ../2_trimgalore/${fq_file2%%.fastq*}_val_2.fq* 2>&1 | tee -a ../${fileID}_logs_${dow}.log
 
-#SE directional on unmapped R1
+## SE directional on unmapped R1
 bismark --multicore 2 $genome_path ${fq_file1%%.fastq*}_val_1.*unmapped_reads_1.fq* 2>&1 | tee -a ../${fileID}_logs_${dow}.log
 
-#SE non-directional on unmapped R2
+## SE non-directional on unmapped R2
 bismark --non_directional $genome_path  ${fq_file2%%.fastq*}_val_2.*unmapped_reads_2.fq* 2>&1 | tee -a ../${fileID}_logs_${dow}.log
 
-#merge & sort SE remapped BAMs
+## merge & sort SE remapped BAMs
 samtools merge ${fq_file1%%.fastq*}_SEremapped.bam ${fq_file1%%.fastq*}*unmapped_reads_1*bt2.bam ${fq_file2%%.fastq*}*unmapped_reads_2*bt2.bam | tee -a ../${fileID}_logs_${dow}.log
 samtools sort ${fq_file1%%.fastq*}_SEremapped.bam -o ${fq_file1%%.fastq*}_SEremapped.sorted.bam | tee -a ../${fileID}_logs_${dow}.log
 samtools index ${fq_file1%%.fastq*}_SEremapped.sorted.bam | tee -a ../${fileID}_logs_${dow}.log
@@ -227,7 +229,7 @@ rm -v *_unmapped_reads_*
 rm -v *SEremapped.bam
 
 #methylation extraction PE (-p)
-bismark_methylation_extractor --comprehensive --report --multicore 2 --buffer_size 8G -p --gzip ${fq_file1%%.fastq*}_val_1.fq*_bismark_bt2_pe.bam 2>&1 | tee -a ../${fileID}_logs_${dow}.log
+bismark_methylation_extractor --comprehensive --report --multicore 2 --buffer_size 8G -p --gzip ${fq_file1%%.fastq*}_val_1_bismark_bt2_pe.bam 2>&1 | tee -a ../${fileID}_logs_${dow}.log
 
 #methylation extraction SE re-mapped
 bismark_methylation_extractor --comprehensive --report --multicore 2 --buffer_size 8G -s --gzip ${fq_file1%%.fastq*}_SEremapped.sorted.bam 2>&1 | tee -a ../${fileID}_logs_${dow}.log
