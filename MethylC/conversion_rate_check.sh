ea2525ecfbb2b0b72bc291efe6c6523dbe793f37
Diep <diep.ganguly@anu.edu.au>
2018-04-19 13:33:20 +1000

    reduce redundant calls

--- MethylC/conversion_rate_check.sh
+++ MethylC/conversion_rate_check.sh
@@ -10,7 +10,8 @@ echo "ChrC"
 grep "ChrC"  ${file} | awk '{ met+= $5} { unmet += $6} { total = met + unmet } END {print 100-((met / total)*100)}'
 echo "ChrM"
 grep "ChrM"  ${file} | awk '{ met+= $5} { unmet += $6} { total = met + unmet } END {print 100-((met / total)*100)}'
-echo "Pt"
-grep "Pt"  ${file} | awk '{ met+= $5} { unmet += $6} { total = met + unmet } END {print 100-((met / total)*100)}'
-echo "Mt"
-grep "Mt"  ${file} | awk '{ met+= $5} { unmet += $6} { total = met + unmet } END {print 100-((met / total)*100)}'
+# echo "Pt"
+# grep "Pt"  ${file} | awk '{ met+= $5} { unmet += $6} { total = met + unmet } END {print 100-((met / total)*100)}'
+# echo "Mt"
+# grep "Mt"  ${file} | awk '{ met+= $5} { unmet += $6} { total = met + unmet } END {print 100-((met / total)*100)}'
+
