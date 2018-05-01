ee1ab89d1a092a92497f1cfbc119b6a1e51e24a4
Diep <diep.ganguly@anu.edu.au>
2018-05-01 17:25:48 +1000

    specify sorted bam input

--- average_cov.sh
+++ average_cov.sh
@@ -1,1 +1,6 @@
-samtools depth  *sorted.bam  |  awk '{sum+=$3} END { print "Average = ",sum/NR}'
+#!/bin/bash
+set -eu
+
+bam=$1
+ 
+samtools depth  $bam  |  awk '{sum+=$3} END { print "Average = ",sum/NR}'
