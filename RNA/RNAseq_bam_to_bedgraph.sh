01ec22c42f98d358bf19227298457de7af73e732
d_train16 <diepganguly@Gmail.com>
2018-05-30 14:34:23 +1000

    updated SE stranded and rev_stranded

--- RNA/RNAseq_bam_to_bedgraph.sh
+++ RNA/RNAseq_bam_to_bedgraph.sh
@@ -10,7 +10,7 @@ set -u
 
 if [ "$#" -lt 3 ]; then
 echo "Missing arguments!"
-echo "USAGE: RNAseq_bam_to_bedgraph.sh <sample name> <SE, PE> <unstranded, stranded>"
+echo "USAGE: RNAseq_bam_to_bedgraph.sh <sample name> <SE, PE> <unstranded, stranded, rev_stranded>"
 echo "EXAMPLE: RNAseq_bam_to_bedgraph.sh col0-r1.bam PE unstranded"
 exit 1
 fi
@@ -20,7 +20,7 @@ lay=$2
 str=$3
 
 # file for length of all 7 chromosomes
-chrc_sizes=/home/diep/TAIR10/TAIR10_Chr.all.fasta.len
+chrc_sizes=$HOME/TAIR10/TAIR10_Chr.all.fasta.len
 
 echo ""
 echo "sample = $1"
@@ -32,87 +32,167 @@ echo ""
 
 if [[ "$lay" == "SE" ]] && [[ "$str"  == "unstranded" ]] ; then 
 
-echo "BAM to bedgraph ..."
-# unstranded bedgraph
-bedtools genomecov -bga -split -ibam $smp -g $chrc_sizes > ${smp%%bam}bg
+	echo "BAM to bedgraph ..."
+	# unstranded bedgraph
+	bedtools genomecov -bga -split -ibam $smp -g $chrc_sizes > ${smp%%bam}bg
 
-# bg to bigWig
-echo "bigWig ..."
-$HOME/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}bg ${chrc_sizes} ${smp%%bam}bigWig
+	# bg to bigWig
+	echo "bigWig ..."
+	$HOME/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}bg ${chrc_sizes} ${smp%%bam}bigWig
 
 fi
 
 if [[ "$lay" == "SE" ]] && [[ "$str"  == "stranded" ]] ; then
-
-echo "Does not exist ... bye"
-# https://www.biostars.org/p/179035/
-# samtools view -f 128 F 4 -b $smp > 
+	# https://www.biostars.org/p/179035/
+	# extract reads from + and - strand
+	
+	# reverse strand
+	samtools view -f 16 -b $smp > ${smp%%bam}reverse.bam
+	# forward strand
+	samtools view -F 16 -b $smp > ${smp%%bam}forward.bam
+	echo "BAM to stranded bedgraphs ..."
+	# reverse/minus bg
+	bedtools genomecov -bga -split -scale -1 -ibam ${smp%%bam}reverse.bam -g $chrc_sizes > ${smp%%bam}minus.bg
+	# forward/plus bg
+	bedtools genomecov -bga -split -ibam ${smp%%bam}forward.bam -g $chrc_sizes > ${smp%%bam}plus.bg
+	
+	echo "bigWigs..."
+	$HOME/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}plus.bg ${chrc_sizes}  ${smp%%bam}plus.bigWig
+	$HOME/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}minus.bg ${chrc_sizes} ${smp%%bam}minus.bigWig
+	
+	rm ${smp%%.bam}*forward*bam -v
+	rm ${smp%%.bam}*reverse*bam -v
 
 fi
 
 
+if [[ "$lay" == "SE" ]] && [[ "$str"  == "rev_stranded" ]] ; then
+	# https://www.biostars.org/p/179035/
+	# extract reads from + and - strand
+
+	# reverse strand
+	samtools view -f 16 -b $smp > ${smp%%bam}reverse.bam
+	# forward strand
+	samtools view -F 16 -b $smp > ${smp%%bam}forward.bam
+	echo "BAM to stranded bedgraphs ..."
+	# reverse/plus bg
+	bedtools genomecov -bga -split -ibam ${smp%%bam}reverse.bam -g $chrc_sizes > ${smp%%bam}plus.bg
+	# forward/minus bg
+	bedtools genomecov -bga -split -scale -1 -ibam ${smp%%bam}forward.bam -g $chrc_sizes > ${smp%%bam}minus.bg
+
+	echo "bigWigs..."
+	$HOME/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}plus.bg ${chrc_sizes}  ${smp%%bam}plus.bigWig
+	$HOME/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}minus.bg ${chrc_sizes} ${smp%%bam}minus.bigWig
+	rm ${smp%%.bam}*forward*bam -v
+	rm ${smp%%.bam}*reverse*bam -v
+
+fi
+
 if [[ "$lay" == "PE" ]] && [[ "$str"  == "unstranded" ]] ; then
+	# need sorted bam
+	echo "sort by position"
+	samtools sort ${smp} -o ${smp%%bam}sorted.bam
+	smp="${smp%%bam}sorted.bam"
+	
+	echo "BAM to bedgraph ..."
+	# unstranded bedgraph
+	bedtools genomecov -bga -split -ibam $smp -g $chrc_sizes > ${smp%%bam}bg
 
-# need sorted bam
-echo "sort by position"
-samtools sort ${smp} -o ${smp%%bam}sorted.bam
+	# bg to bigWig
+	echo "bigWig ..."
+	$HOME/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}bg ${chrc_sizes} ${smp%%bam}bigWig
 
-smp="${smp%%bam}sorted.bam"
-
-echo "BAM to bedgraph ..."
-# unstranded bedgraph
-bedtools genomecov -bga -split -ibam $smp -g $chrc_sizes > ${smp%%bam}bg
-
-# bg to bigWig
-echo "bigWig ..."
-$HOME/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}bg ${chrc_sizes} ${smp%%bam}bigWig
-
-rm $smp -v
+	rm $smp -v
 
 fi
 
 if [[ "$lay" == "PE" ]] && [[ "$str"  == "stranded" ]] ; then
 
-echo "Extract properly-paired read mates (+ flags 99/147; - flags 83/163) from paired-end BAM files"
-# http://seqanswers.com/forums/showthread.php?t=29399
+	echo "Extract properly-paired read mates (+ flags 99/147; - flags 83/163) from paired-end BAM files"
+	# http://seqanswers.com/forums/showthread.php?t=29399
+	
+	# need sorted bam
+	echo "sort by position"
+	samtools sort ${smp} -o ${smp%%bam}sorted.bam
 
-# need sorted bam
-echo "sort by position"
-samtools sort ${smp} -o ${smp%%bam}sorted.bam
+	smp="${smp%%bam}sorted.bam"
 
-smp="${smp%%bam}sorted.bam"
+	echo "forward strand"
+	# R1 forward
+	samtools view -f 99 -b $smp > ${smp%%bam}R1F.bam
+	# R2 reverse
+	samtools view -f 147 -b $smp > ${smp%%bam}R2R.bam
+	# FORWARD R1 read pairs
+	samtools merge -f ${smp%%bam}forward.bam ${smp%%bam}R1F.bam ${smp%%bam}R2R.bam
 
-echo "forward strand"
-# R1 forward
-samtools view -f 99 -b $smp > ${smp%%bam}R1F.bam
-# R2 reverse
-samtools view -f 147 -b $smp > ${smp%%bam}R2R.bam
-# FORWARD R1 read pairs
-samtools merge -f ${smp%%bam}forward.bam ${smp%%bam}R1F.bam ${smp%%bam}R2R.bam
+	echo "reverse strand"
+	# R1 reverse
+	samtools view -f 83 -b $smp > ${smp%%bam}R1R.bam
+	# R2 forward
+	samtools view -f 163 -b $smp > ${smp%%bam}R2F.bam
+	# REVERSE R1 read pairs
+	samtools merge -f ${smp%%bam}reverse.bam ${smp%%bam}R1R.bam ${smp%%bam}R2F.bam
 
-echo "reverse strand"
-# R1 reverse
-samtools view -f 83 -b $smp > ${smp%%bam}R1R.bam
-# R2 forward
-samtools view -f 163 -b $smp > ${smp%%bam}R2F.bam
-# REVERSE R1 read pairs
-samtools merge -f ${smp%%bam}reverse.bam ${smp%%bam}R1R.bam ${smp%%bam}R2F.bam
+	echo "BAM to stranded bedgraph ..."
+	# minus strand
+	bedtools genomecov -bga -split -scale -1 -ibam ${smp%%bam}reverse.bam -g $chrc_sizes > ${smp%%bam}minus.bg
+	# plus strand
+	bedtools genomecov -bga -split -ibam ${smp%%bam}forward.bam -g $chrc_sizes > ${smp%%bam}plus.bg
 
-echo "BAM to stranded bedgraph ..."
-# plus strand
-bedtools genomecov -bga -split -ibam ${smp%%bam}reverse.bam -g $chrc_sizes > ${smp%%bam}plus.bg
-# minus strand
-bedtools genomecov -bga -split -scale -1 -ibam ${smp%%bam}forward.bam -g $chrc_sizes > ${smp%%bam}minus.bg
+	echo "bigWigs..."
+	$HOME/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}plus.bg ${chrc_sizes}  ${smp%%bam}plus.bigWig
+	$HOME/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}minus.bg ${chrc_sizes} ${smp%%bam}minus.bigWig
 
-echo "bigWigs..."
-$HOME/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}plus.bg ${chrc_sizes}  ${smp%%bam}plus.bigWig
-$HOME/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}minus.bg ${chrc_sizes} ${smp%%bam}minus.bigWig
+	rm ${smp%%.bam}*R1*bam -v
+	rm ${smp%%.bam}*R2*bam -v
+	rm ${smp%%.bam}*forward*bam -v
+	rm ${smp%%.bam}*reverse*bam -v
+	rm $smp -v
 
-rm ${smp%%.bam}*R1*bam -v
-rm ${smp%%.bam}*R2*bam -v
-rm ${smp%%.bam}*forward*bam -v
-rm ${smp%%.bam}*reverse*bam -v
-rm $smp -v
+fi
+
+if [[ "$lay" == "PE" ]] && [[ "$str"  == "rev_stranded" ]] ; then
+
+	echo "Extract properly-paired read mates (+ flags 99/147; - flags 83/163) from paired-end BAM files"
+	# http://seqanswers.com/forums/showthread.php?t=29399
+
+	# need sorted bam
+	echo "sort by position"
+	samtools sort ${smp} -o ${smp%%bam}sorted.bam
+
+	smp="${smp%%bam}sorted.bam"
+
+	echo "forward strand"
+	# R1 forward
+	samtools view -f 99 -b $smp > ${smp%%bam}R1F.bam
+	# R2 reverse
+	samtools view -f 147 -b $smp > ${smp%%bam}R2R.bam
+	# FORWARD R1 read pairs
+	samtools merge -f ${smp%%bam}forward.bam ${smp%%bam}R1F.bam ${smp%%bam}R2R.bam
+
+	echo "reverse strand"
+	# R1 reverse
+	samtools view -f 83 -b $smp > ${smp%%bam}R1R.bam
+	# R2 forward
+	samtools view -f 163 -b $smp > ${smp%%bam}R2F.bam
+	# REVERSE R1 read pairs
+	samtools merge -f ${smp%%bam}reverse.bam ${smp%%bam}R1R.bam ${smp%%bam}R2F.bam
+
+	echo "BAM to stranded bedgraph ..."
+	# plus strand
+	bedtools genomecov -bga -split -ibam ${smp%%bam}reverse.bam -g $chrc_sizes > ${smp%%bam}plus.bg
+	# minus strand
+	bedtools genomecov -bga -split -scale -1 -ibam ${smp%%bam}forward.bam -g $chrc_sizes > ${smp%%bam}minus.bg
+
+	echo "bigWigs..."
+	$HOME/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}plus.bg ${chrc_sizes}  ${smp%%bam}plus.bigWig
+	$HOME/bin/kentUtils/bin/bedGraphToBigWig ${smp%%bam}minus.bg ${chrc_sizes} ${smp%%bam}minus.bigWig
+
+	rm ${smp%%.bam}*R1*bam -v
+	rm ${smp%%.bam}*R2*bam -v
+	rm ${smp%%.bam}*forward*bam -v
+	rm ${smp%%.bam}*reverse*bam -v
+	rm $smp -v
 
 fi
 
