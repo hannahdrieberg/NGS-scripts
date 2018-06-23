3479b55020ee19a0c4b5dfe9dcc4652d4f730b69
d_train16 <diepganguly@Gmail.com>
2018-06-23 15:01:11 +1000

    calculating weighted methylation levels, removed intermediate files

--- MethylC/wgbs_custom_bins.sh
+++ MethylC/wgbs_custom_bins.sh
@@ -26,24 +26,19 @@ samtools faidx $fas
 awk -v OFS='\t' {'print $1,$2'} ${fas}.fai > temp.genome
 
 # use genome file to make 100bp windows across genome
-bedtools makewindows -g temp.genome -w ${window} -s ${bin} > temp.genome.${bin}bp.bed
-sortBed -i temp.genome.${bin}bp.bed | awk -F$'\t' ' $1 != "ChrC" && $1 != "ChrM" ' > temp.genome.${bin}bp.sorted.bed
+bedtools makewindows -g temp.genome -w ${window} -s ${bin} | sortBed | awk -F$'\t' ' $1 != "ChrC" && $1 != "ChrM" ' > temp.genome.${bin}bp.sorted.bed
 
 # use bedtool intersect and groupBy to get mean methylation levels per bin based on per-site methylation
 echo 'Bedtools CG ...'
-bedtools intersect -sorted -a temp.genome.${bin}bp.bed -b ${bed}_CG.bed.bismark.cov > ${bed}_CG_${bin}bp.bed -wo
-groupBy -i ${bed}_CG_${bin}bp.bed -g 1,2,3 -c 7,7 -o mean,count > ${bed}_CG_${bin}bp.avg.bed
+bedtools intersect -sorted -wo -a temp.genome.${bin}bp.sorted.bed -b ${bed}_CG.bed.bismark.cov | groupBy -g 1,2,3 -c 7,8,9 -o mean,sum,sum | awk -v OFS='\t' '{print $1,$2,$3,$4 = ($5 / ($5+$6)*100 )}' > ${bed}_CG_${bin}bp.bed
 
 echo 'Bedtools CHG ...'
-bedtools intersect -sorted -a temp.genome.${bin}bp.bed -b ${bed}_CHG.bed.bismark.cov > ${bed}_CHG_${bin}bp.bed -wo
-groupBy -i ${bed}_CHG_${bin}bp.bed -g 1,2,3 -c 7,7 -o mean,count > ${bed}_CHG_${bin}bp.avg.bed
+bedtools intersect -sorted -wo -a temp.genome.${bin}bp.sorted.bed -b ${bed}_CHG.bed.bismark.cov | groupBy -g 1,2,3 -c 7,8,9 -o mean,sum,sum | awk -v OFS='\t' '{print $1,$2,$3,$4 = ($5 / ($5+$6)*100 )}' > ${bed}_CHG_${bin}bp.bed
 
 echo 'Bedtools CHH ...'
-bedtools intersect -sorted -a temp.genome.${bin}bp.bed -b ${bed}_CHH.bed.bismark.cov > ${bed}_CHH_${bin}bp.bed -wo
-groupBy -i ${bed}_CHH_${bin}bp.bed -g 1,2,3 -c 7,7 -o mean,count > ${bed}_CHH_${bin}bp.avg.bed
+bedtools intersect -sorted -wo -a temp.genome.${bin}bp.sorted.bed -b ${bed}_CHH.bed.bismark.cov | groupBy -g 1,2,3 -c 7,8,9 -o mean,sum,sum | awk -v OFS='\t' '{print $1,$2,$3,$4 = ($5 / ($5+$6)*100 )}' > ${bed}_CHH_${bin}bp.bed
 
 echo 'cleaning ...'
 # CLEAN
 rm temp.genome*
-rm *_${bin}bp.bed
 
