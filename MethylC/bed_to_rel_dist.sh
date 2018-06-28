6e751cd3861234c7675483f51cff2ff08aa846b6
d_train16 <diepganguly@Gmail.com>
2018-06-28 16:22:39 +1000

    filename pedantics

--- MethylC/bed_to_rel_dist.sh
+++ MethylC/bed_to_rel_dist.sh
@@ -21,18 +21,18 @@ outname=$4
 
 if [ "$1" == "-wig" ];then
 
-sed -e "1d" ${filename}_CpG_100bp.wig > ${filename}_CpG_100bp.bed
+sed -e "1d" ${filename}_CpG_100bp.wig > ${filename}_CG_100bp.bed
 sed -e "1d" ${filename}_CHG_100bp.wig > ${filename}_CHG_100bp.bed
 sed -e "1d" ${filename}_CHH_100bp.wig > ${filename}_CHH_100bp.bed
 
 fi
 
-sort -k1,1 -k2,2n ${filename}_CpG_100bp.bed -o ${filename}_CpG_100bp.bed
+sort -k1,1 -k2,2n ${filename}_CG_100bp.bed -o ${filename}_CG_100bp.bed
 sort -k1,1 -k2,2n ${filename}_CHG_100bp.bed -o ${filename}_CHG_100bp.bed
 sort -k1,1 -k2,2n ${filename}_CHH_100bp.bed -o ${filename}_CHH_100bp.bed
 
 #get total number of columns for both input files
-l1="$(cat ${filename}_CpG_100bp.bed | awk 'BEGIN{FS="\t"};{print NF}' | head -n 1)"
+l1="$(cat ${filename}_CG_100bp.bed | awk 'BEGIN{FS="\t"};{print NF}' | head -n 1)"
 l2="$(cat ${bedpath} | awk 'BEGIN{FS="\t"};{print NF}' | head -n 1)"
 
 #convert the wigs to bed
@@ -42,13 +42,13 @@ closestBed -D "ref" -a ${filename}_CHG_100bp.bed -b $bedpath > ${filename}_CHG_$
 echo "Performing closestBed of CHH methylation..."
 closestBed -D "ref" -a ${filename}_CHH_100bp.bed -b $bedpath > ${filename}_CHH_${outname}.bed
 echo "Performing closestBed of CpG methylation..."
-closestBed -D "ref" -a ${filename}_CpG_100bp.bed -b $bedpath > ${filename}_CpG_${outname}.bed
+closestBed -D "ref" -a ${filename}_CG_100bp.bed -b $bedpath > ${filename}_CG_${outname}.bed
 
 #subset to the regions within 100bp of a gene (make the files more manageable for R)
 echo "subsetting files to within 1kb..."
 awk -F$'\t' '$NF<1000 && $NF>-1000' ${filename}_CHG_${outname}.bed > ${filename}_CHG_${outname}.1k.bed
 awk -F$'\t' '$NF<1000 && $NF>-1000' ${filename}_CHH_${outname}.bed > ${filename}_CHH_${outname}.1k.bed
-awk -F$'\t' '$NF<1000 && $NF>-1000' ${filename}_CpG_${outname}.bed > ${filename}_CpG_${outname}.1k.bed
+awk -F$'\t' '$NF<1000 && $NF>-1000' ${filename}_CG_${outname}.bed > ${filename}_CG_${outname}.1k.bed
 #######################
 
 echo "Performing R plots..."
