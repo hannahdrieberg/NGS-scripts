70bd7c8e82aed9ca96790f005223bac7c1de80a9
d_train16 <diepganguly@Gmail.com>
2018-08-01 16:01:43 +1000

    threads

--- average_cov.sh
+++ average_cov.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -eu
-
 bam=$1
-
-samtools --threads 4 sort $bam | samtools depth - | awk '{sum+=$3} END { print "Mean depth = ",sum/NR}'
+samtools sort -@ 4 $bam | samtools depth - | awk '{sum+=$3} END { print "Mean depth = ",sum/NR}'
